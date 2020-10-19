4e805bd49628d705011553ccb945b7188fa638c3
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2020-10-19 22:13:42 +0200

    Eeprom generator

--- TableFirmware/Eeprom/gen.py
+++ TableFirmware/Eeprom/gen.py
@@ -5,6 +5,7 @@ f = ih.IntelHex()
 for addr in range(0, 0x400):
 	f[addr] = 0xff
 
+# photo modules
 for row in range(1, 5):
 	for col in range(1, 7):
 		d = 0x10 * row + col
@@ -13,3 +14,10 @@ for row in range(1, 5):
 		f[2] = d
 	
 		f.write_hex_file(f"{d:02x}.hex")
+
+# trigger module
+f[0] = 0x1f
+f[1] = 0x1f
+f[2] = 0x1f
+d = 0x1f
+f.write_hex_file(f"{d:02x}.hex")
